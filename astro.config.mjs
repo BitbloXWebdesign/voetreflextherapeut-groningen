2e6823b9f7482430a687966da410ef1e86c2b7e4
BitbloX Agency <info@bitblox.nl>
2026-08-27 20:26:36 +0200

    Ensure clean static sitemap build and push to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://www.voetreflextherapeutgroningen.nl',
-  integrations: [sitemap()]
+  site: 'https://www.voetreflextherapeutgroningen.nl'
 });
